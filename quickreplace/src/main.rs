b22dd5d20b82d20a515e07023a125af3c898dea9
vogonwann <wannjanjic@gmail.com>
2023-04-30 18:54:36 +0200

    add crates, add printing of command usage and parsing arguments

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,38 @@
+use text_colorizer::*;
+use std::env;
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+    println!("{:?}", args);
+}
+
+fn print_usage() {
+    eprintln!("{} - change occurrences of one string into another", "quickreplace".green());
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!("{} wrong number of arguments: expected 4, got {}.", 
+            "Error:".red().bold(), args.len());
+        std::process::exit(1);
+    }
+
+    Arguments { 
+        target: args[0].clone(), 
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone()
+    }
 }
